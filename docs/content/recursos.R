c2b90de703e0b0cea1da411e5fe680e9e33f97ee
Juan Pablo Ruiz Nicolini <jruiznicolini@turismo.gob.ar>
2021-10-14 20:35:34 -0300

    incorporo reporte MULC a tarjetas

--- docs/content/recursos.R
+++ docs/content/recursos.R
@@ -3,26 +3,28 @@
 library(tidyverse)
 
 data.frame(
-  recurso = c("Turismo Internacional", "Ocupación Hotelera", "Naturaleza", "EVYTH", "Explorador", "PUNA"), 
-  src = c("img/ti.png", "img/eoh.png","img/parques.png","img/evyth.png","img/ti_dash.png" , "img/puna.png"), 
+  recurso = c("Turismo Internacional", "Ocupación Hotelera", "Naturaleza", "EVYTH", "Explorador", "PUNA", "MULC"), 
+  src = c("img/ti.png", "img/eoh.png","img/parques.png","img/evyth.png","img/ti_dash.png" , "img/puna.png", "img/mulc.png"), 
   url = c("https://tableros.yvera.tur.ar/internacional.html",
           "https://tableros.yvera.tur.ar/eoh.html",
           "https://tableros.yvera.tur.ar/parques_nacionales.html",
           "https://tableros.yvera.tur.ar/interno.html",
           "https://tableros.yvera.tur.ar/turismo_internacional/",
-          "https://tableros.yvera.tur.ar/puna/"), 
+          "https://tableros.yvera.tur.ar/puna/",
+          "https://tableros.yvera.tur.ar/mulc.html"), 
   description  = c("El reporte presenta la última información de turismo receptivo, emisivo y balanza turística 
                    por todas las vías (aéreo, terrestre y fluvial / marítimo)",
                    "Presenta los últimos datos disponibles de la Encuesta de Ocupación Hotelera (EOH)",
                    "Información de visitas a Parques Nacionales",
                    "Reporte de los últimos datos de Turismo Interno estimados por la Encuesta de Viajes y Turismo de los Hogares",
                    "Tablero para la consulta de datos desagregados de las estimaciones históricas de Turismo Internacional",
-                   "Tablero para la consulta de estimaciones agregadas del Padrón Único Nacional de Alojamientos (PUNA)"), 
-  tags = c("Reporte","Reporte","Reporte","Reporte","Tablero", "Tablero"),
-  Todos = c(1, 1, 1, 1, 1, 1), 
-  Tablero = c(0, 0, 0, 0, 1, 1), 
-  Reporte = c(1, 1, 1, 1, 0, 0)) %>% 
+                   "Tablero para la consulta de estimaciones agregadas del Padrón Único Nacional de Alojamientos (PUNA)",
+                   "Reporte sobre transacciones en divisas relacionadas al turismo en el Mercado Único y Libre de Cambios (MULC)"), 
+  tags = c("Reporte","Reporte","Reporte","Reporte","Tablero", "Tablero", "Reporte"),
+  Todos = c(1, 1, 1, 1, 1, 1, 1), 
+  Tablero = c(0, 0, 0, 0, 1, 1, 0), 
+  Reporte = c(1, 1, 1, 1, 0, 0, 1)) %>% 
   as_tibble() %>% 
-  mutate(orden = c(1, 5, 4, 3, 2, 6)) %>%   # OREDNO POR TEMA Y NO POR TIPO DE RECURSO 
+  mutate(orden = c(1, 5, 4, 3, 2, 6, 7)) %>%   # OREDNO POR TEMA Y NO POR TIPO DE RECURSO 
   arrange(orden)  %>% 
   write_csv(here::here("content", "recursos.csv"))
