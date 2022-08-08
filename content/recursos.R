fd2f50fbad25dd736b98261eec8e5ae1d07d6019
Juan Pablo Ruiz Nicolini <jruiznicolini@turismo.gob.ar>
2022-08-08 18:41:05 -0300

    agrego tablero AGENCIAS

--- content/recursos.R
+++ content/recursos.R
@@ -3,8 +3,10 @@
 library(tidyverse)
 
 data.frame(
-  recurso = c("Turismo Internacional", "Ocupación Hotelera", "Naturaleza", "EVYTH", "Explorador", "PUNA", "MULC", "Empleo", "mapeAr"),
-  src = c("img/ti.png", "img/eoh.png","img/parques.png","img/evyth.png","img/ti_dash.png" , "img/puna.png", "img/mulc.png", "img/empleo.png", "img/mapeAr.png"),
+  recurso = c("Turismo Internacional", "Ocupación Hotelera", "Naturaleza", "EVYTH", "Explorador",
+              "PUNA", "MULC", "Empleo", "mapeAr", "Agencias"),
+  src = c("img/ti.png", "img/eoh.png","img/parques.png","img/evyth.png","img/ti_dash.png" ,
+          "img/puna.png", "img/mulc.png", "img/empleo.png", "img/mapeAr.png", "img/agencias.png"),
   url = c("https://tableros.yvera.tur.ar/internacional.html",
           "https://tableros.yvera.tur.ar/eoh.html",
           "https://tableros.yvera.tur.ar/parques_nacionales.html",
@@ -13,7 +15,8 @@ data.frame(
           "https://tableros.yvera.tur.ar/puna/",
           "https://tableros.yvera.tur.ar/mulc.html",
           "https://tableros.yvera.tur.ar/empleo.html",
-          "https://tableros.yvera.tur.ar/mapeAr"),
+          "https://tableros.yvera.tur.ar/mapeAr",
+          "https://tableros.yvera.tur.ar/agencias"),
   description  = c("El reporte presenta la última información de turismo receptivo, emisivo y balanza turística
                    por todas las vías (aéreo, terrestre y fluvial / marítimo)",
                    "Presenta los últimos datos disponibles de la Encuesta de Ocupación Hotelera (EOH)",
@@ -23,12 +26,13 @@ data.frame(
                    "Tablero para la consulta de estimaciones agregadas del Padrón Único Nacional de Alojamientos (PUNA)",
                    "Reporte sobre transacciones en divisas relacionadas al turismo en el Mercado Único y Libre de Cambios (MULC)",
                    "Reporte sobre empleo registrado en las ramas características del turismo",
-                   "Tablero para la generación de mapas con información georeferenciada de Turismo"),
-  tags = c("Reporte","Reporte","Reporte","Reporte","Tablero", "Tablero", "Reporte", "Reporte", "Tablero"),
-  Todos = c(1, 1, 1, 1, 1, 1, 1, 1, 1),
-  Tablero = c(0, 0, 0, 0, 1, 1, 0, 0, 1),
-  Reporte = c(1, 1, 1, 1, 0, 0, 1, 1, 0)) %>%
+                   "Tablero para la generación de mapas con información georeferenciada de Turismo",
+                   "Tablero con información del Registro de Agencias de Viajes de la Dirección Nacional de Agencias de Viajes"),
+  tags = c("Reporte","Reporte","Reporte","Reporte","Tablero", "Tablero", "Reporte", "Reporte", "Tablero", "Tablero"),
+  Todos = c(1, 1, 1, 1, 1, 1, 1, 1, 1,1),
+  Tablero = c(0, 0, 0, 0, 1, 1, 0, 0, 1,1),
+  Reporte = c(1, 1, 1, 1, 0, 0, 1, 1, 0,0)) %>%
   as_tibble() %>%
-  mutate(orden = c(1, 5, 4, 3, 2, 6, 7, 8, 9)) %>%   # OREDNO POR TEMA Y NO POR TIPO DE RECURSO
+  mutate(orden = c(1, 5, 4, 3, 2, 6, 7, 8, 9, 0)) %>%   # OREDNO POR TEMA Y NO POR TIPO DE RECURSO
   arrange(orden)  %>%
   write_csv(here::here("content", "recursos.csv"))
